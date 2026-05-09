0db0bf51d85c1e06a58f23f6e646c374249c67be
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-09 11:19:58 +0200

    feat: added diagnostic codes

--- compiler/calsc_diagnostics/src/lib.rs
+++ compiler/calsc_diagnostics/src/lib.rs
@@ -4,3 +4,39 @@
 
 pub mod fmt;
 pub mod span;
+
+/// The level of diagnostics. Represents the type of diagnostic (eg: error, warning or information).
+pub enum Level {
+    Error,
+    Warning,
+    Info,
+}
+
+/// Represents a code to represent diagnostics from each other. Also called a warning / error code.
+/// Contains the diagnostic level and a code representing the diagnostic.
+///
+/// # Example
+///
+pub struct DiagnosticCode {
+    pub level: Level,
+    pub code: usize,
+}
+
+pub struct Diagnostic {
+    pub code: DiagnosticCode,
+}
+
+impl DiagnosticCode {
+    /// Creates a new diagnostic code with the given diagnostic level and the given warn / error code.
+    ///
+    /// # Example
+    /// ```
+    /// use calsc_diagnostics::{Level, DiagnosticCode};
+    ///
+    /// // Represents the diagnostic "E123"
+    /// let code: DiagnosticCode = DiagnosticCode::new(Level::Error, 123);
+    /// ```
+    pub fn new(level: Level, code: usize) -> Self {
+        Self { level, code }
+    }
+}
