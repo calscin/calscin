f75b139ed7029bb35bf23509e0e1e4b463c5a679
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-10 17:27:24 +0200

    feat: added expected error

--- compiler/calsc_diagnostics/src/diags/errors.rs
+++ compiler/calsc_diagnostics/src/diags/errors.rs
@@ -7,6 +7,7 @@ use crate::{Diagnostic, DiagnosticCode, DiagnosticSource, Level, declare_diagnos
 // Is triggered whenever the Lexer cannot parse something (eg: cannot parse literals).
 declare_diagnostic!(CANNOT_PARSE, 1);
 declare_diagnostic!(UNEXPECTED_TOKEN, 2);
+declare_diagnostic!(EXPECTED, 3);
 
 /// Builds a `CANNOT_PARSE` error (E1) based on the given source and given element.
 pub fn build_cannot_parse_error<P: Display, S: DiagnosticSource>(p: &P, source: &S) -> Diagnostic {
@@ -23,7 +24,22 @@ pub fn build_cannot_parse_error<P: Display, S: DiagnosticSource>(p: &P, source: 
 pub fn build_unexpected_error<E: Display, S: DiagnosticSource>(elem: &E, source: &S) -> Diagnostic {
     source.make_diagnostic_simple(
         DiagnosticCode::new(Level::Error, UNEXPECTED_TOKEN),
-        format!("unexpected token {}", elem),
+        format!("unexpected {}", elem),
+        None,
+        vec![],
+        vec![],
+        vec![],
+    )
+}
+
+pub fn build_expected_error<E: Display, G: Display, S: DiagnosticSource>(
+    expected: &E,
+    got: &G,
+    source: &S,
+) -> Diagnostic {
+    source.make_diagnostic_simple(
+        DiagnosticCode::new(Level::Error, UNEXPECTED_TOKEN),
+        format!("expected {} but got {}", expected, got),
         None,
         vec![],
         vec![],
