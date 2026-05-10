b45d40992f8dfb9e6014b2b975bc5a673aa94717
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-10 08:58:49 +0200

    feat: added cannot parse builder

--- compiler/calsc_diagnostics/src/diags/errors.rs
+++ compiler/calsc_diagnostics/src/diags/errors.rs
@@ -1,6 +1,20 @@
 //! The error declarations
 
-use crate::declare_diagnostic;
+use std::fmt::Display;
+
+use crate::{Diagnostic, DiagnosticCode, DiagnosticSource, Level, declare_diagnostic};
 
 // Is triggered whenever the Lexer cannot parse something (eg: cannot parse literals).
 declare_diagnostic!(CANNOT_PARSE, 1);
+
+/// Builds a `CANNOT_PARSE` error (E1) based on the given source and given element.
+pub fn build_cannot_parse_error<P: Display, S: DiagnosticSource>(p: &P, source: &S) -> Diagnostic {
+    source.make_diagnostic_simple(
+        DiagnosticCode::new(Level::Error, CANNOT_PARSE),
+        format!("cannot parse {}", p),
+        None,
+        vec![],
+        vec![],
+        vec![],
+    )
+}
