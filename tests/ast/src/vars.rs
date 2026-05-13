93822aba8ca26ea7084e246af31af9875e1f77dd
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-13 14:02:58 +0200

    fix: added a missing [test]

--- tests/ast/src/vars.rs
+++ tests/ast/src/vars.rs
@@ -10,6 +10,7 @@ pub fn test_parse_variable_delc_no_def() {
     let _ = parse_ast_variable_declaration(&tokens, &mut ind).unwrap_cleanly();
 }
 
+#[test]
 pub fn test_parse_variable_delc_def() {
     let tokens = lexer_tokenize("var s32 test = 45", "test.cal".to_string()).unwrap_cleanly();
     let mut ind = 0;
