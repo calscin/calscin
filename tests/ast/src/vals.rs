e315d9bfdfb4bc6589e296e98c3c00d9992d8d76
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-15 12:53:54 +0200

    chore: added tests for math expressions

--- tests/ast/src/vals.rs
+++ tests/ast/src/vals.rs
@@ -1,6 +1,10 @@
-use calsc_ast::parser::values::parse_ast_value;
+use calsc_ast::{nodes::ASTNodeKind, parser::values::parse_ast_value};
 use calsc_diagnostics::result::CalscinResult;
 use calsc_lexer::lexer_tokenize;
+use calsc_utils::{
+    hash::HashedString,
+    math::{MathOperation, MathOperator},
+};
 
 #[test]
 pub fn parse_structured_init_test() {
@@ -16,4 +20,50 @@ pub fn parse_malformed_structured_init_test() {
     let mut ind = 0;
 
     let _ = parse_ast_value(&tokens, &mut ind, true, false).unwrap_err();
+}
+
+#[test]
+pub fn parse_math_operation_test() {
+    let tokens = lexer_tokenize("test += 58", "test.cal".to_string()).unwrap_cleanly();
+    let mut ind = 0;
+
+    let math = parse_ast_value(&tokens, &mut ind, true, false).unwrap_cleanly();
+
+    if let ASTNodeKind::MathExpression {
+        left_expr,
+        right_expr,
+        operator,
+    } = math.kind
+    {
+        assert_eq!(
+            left_expr.kind,
+            ASTNodeKind::VariableReference(HashedString::new("test".to_string()))
+        );
+        assert_eq!(right_expr.kind, ASTNodeKind::IntLiteral(58));
+
+        assert_eq!(operator, MathOperator::new(MathOperation::Add, false, true))
+    }
+}
+
+#[test]
+pub fn parse_math_operation_long_test() {
+    let tokens = lexer_tokenize("test ~++= 58", "test.cal".to_string()).unwrap_cleanly();
+    let mut ind = 0;
+
+    let math = parse_ast_value(&tokens, &mut ind, true, false).unwrap_cleanly();
+
+    if let ASTNodeKind::MathExpression {
+        left_expr,
+        right_expr,
+        operator,
+    } = math.kind
+    {
+        assert_eq!(
+            left_expr.kind,
+            ASTNodeKind::VariableReference(HashedString::new("test".to_string()))
+        );
+        assert_eq!(right_expr.kind, ASTNodeKind::IntLiteral(58));
+
+        assert_eq!(operator, MathOperator::new(MathOperation::And, true, true))
+    }
 }
