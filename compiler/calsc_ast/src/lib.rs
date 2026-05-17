68196bfa1e7b5e07786686ffb30c790941c71085
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-17 15:10:44 +0200

    feat: added arena allocator for AST

--- compiler/calsc_ast/src/lib.rs
+++ compiler/calsc_ast/src/lib.rs
@@ -1,5 +1,9 @@
 //! The main AST declarations of Calscin. AST is used to lower the lexer tokens into parsed structures.
 
+use calsc_utils::alloc::arena::ArenaAllocator;
+
+use crate::nodes::ASTNode;
+
 pub mod ifs;
 pub mod imports;
 pub mod nodes;
@@ -7,3 +11,8 @@ pub mod types;
 
 #[cfg(feature = "parser")]
 pub mod parser;
+
+/// The context of the AST, is used to share things around inside of the AST process
+pub struct ASTContext {
+    pub nodes: ArenaAllocator<ASTNode>,
+}
