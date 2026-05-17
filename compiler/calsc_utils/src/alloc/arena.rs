78d35f86c92e1490219477d9f57f8e6919e5eec6
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-17 15:13:54 +0200

    docs: Aadded docs for ArenaAllocator

--- compiler/calsc_utils/src/alloc/arena.rs
+++ compiler/calsc_utils/src/alloc/arena.rs
@@ -1,3 +1,8 @@
+//! Arena allocator definitions
+
+/// An arena allocator. Handles storing elements and handing out a reference index
+///
+/// https://en.wikipedia.org/wiki/Region-based_memory_management
 pub struct ArenaAllocator<K> {
     arena: Vec<K>,
 }
@@ -5,16 +10,19 @@ pub struct ArenaAllocator<K> {
 pub type ArenaAllocatorReference = usize;
 
 impl<K> ArenaAllocator<K> {
+    /// Creates a new instance of an [`ArenaAllocator`]
     pub fn new() -> Self {
         Self { arena: vec![] }
     }
 
+    /// Creates a new [`ArenaAllocator`] with a given capacity.
     pub fn with_capacity(capacity: usize) -> Self {
         Self {
             arena: Vec::with_capacity(capacity),
         }
     }
 
+    /// Appends a new element of type `K` inside of the Arena allocator and hands out a given reference index.
     pub fn append(&mut self, item: K) -> ArenaAllocatorReference {
         let reference = self.arena.len();
 
@@ -23,12 +31,14 @@ impl<K> ArenaAllocator<K> {
         reference
     }
 
+    /// Gets the reference of the stored object of the given reference index as a reference
     pub fn get(&self, refer: ArenaAllocatorReference) -> &K {
         &self.arena[refer]
     }
 }
 
 impl<K: Clone> ArenaAllocator<K> {
+    /// Clones the stored object of the given reference index
     pub fn get_cloned(&self, refer: ArenaAllocatorReference) -> K {
         self.arena[refer].clone()
     }
