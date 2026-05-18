5869b81d2e9d44e5bb12989aca9cc8db925eb87d
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-18 14:58:00 +0200

    feat: added base type

--- compiler/calsc_typing/src/base/mod.rs
+++ compiler/calsc_typing/src/base/mod.rs
@@ -1,3 +1,22 @@
 //! Definitions for base types. They are also named generics inside of the typing system.
 
+use crate::base::kind::BaseTypeKind;
+
 pub mod kind;
+
+pub struct BaseType {
+    /// The kind of the base type
+    pub kind: BaseTypeKind,
+
+    /// The size specifiers of the type
+    pub size_specifiers: Vec<usize>,
+}
+
+impl BaseType {
+    pub fn new(kind: BaseTypeKind, size_specifiers: Vec<usize>) -> Self {
+        Self {
+            kind,
+            size_specifiers,
+        }
+    }
+}
