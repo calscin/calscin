470791050cc7a75b3f843ed9e888da705af57c43
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-18 15:31:14 +0200

    feat: added type parameters to base type

--- compiler/calsc_typing/src/base/mod.rs
+++ compiler/calsc_typing/src/base/mod.rs
@@ -1,6 +1,6 @@
 //! Definitions for base types. They are also named generics inside of the typing system.
 
-use crate::base::kind::BaseTypeKind;
+use crate::{base::kind::BaseTypeKind, tree::Type};
 
 pub mod kind;
 
@@ -10,19 +10,27 @@ pub struct BaseType {
 
     /// The size specifiers of the type
     pub size_specifiers: Vec<usize>,
+
+    /// The type parameters of the type
+    pub type_parameters: Vec<Type>,
 }
 
 impl BaseType {
-    /// Creates a new [`BaseType`] instance with the given kind and the given type specifiers.
+    /// Creates a new [`BaseType`] instance with the given kind and the given type and size specifiers.
     ///
     /// # Panics
     /// This function will panic if the amount ofsize specifiers aren't equal to the amount required.
     ///
-    pub fn new(kind: BaseTypeKind, size_specifiers: Vec<usize>) -> Self {
+    pub fn new(
+        kind: BaseTypeKind,
+        size_specifiers: Vec<usize>,
+        type_parameters: Vec<Type>,
+    ) -> Self {
         if size_specifiers.len() == kind.get_required_size_parameters() {
             Self {
                 kind,
                 size_specifiers,
+                type_parameters,
             }
         } else {
             panic!(
