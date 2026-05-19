2c62bf584555c3126202468ef3d4d3b78a597857
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-19 10:59:44 +0200

    feat: TypeParameterHaving trait

--- compiler/calsc_typing/src/lib.rs
+++ compiler/calsc_typing/src/lib.rs
@@ -37,3 +37,17 @@ pub trait MutableFieldHavingType {
         source: &K,
     ) -> DiagPossible;
 }
+
+/// Represents a type that can have type parameters
+pub trait TypeParameterHaving {
+    /// Checks if the type has a type parameter of the given name
+    fn has_type_parameter(&self, name: HashedString) -> bool;
+
+    /// Gets the type parameter's type in order to use it based on the given name.
+    ///
+    /// # Panics
+    /// This function will panic if the type parameter doesn't exist in the type.
+    /// Make sure to use [`has_type_parameter`][`TypeParameterHaving::has_type_parameter`] first.
+    ///
+    fn get_type_parameter_type(&self, name: HashedString) -> Type;
+}
