e9540e7c6ce4fa40217492f4fe390e3ad3187668
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-20 14:49:18 +0200

    feat: added DeclBlockAffectedType to Type

--- compiler/calsc_typing/src/tree.rs
+++ compiler/calsc_typing/src/tree.rs
@@ -1,8 +1,13 @@
 //! Declarations for the type tree
 
+use calsc_diagnostics::{DiagPossible, DiagnosticSource};
 use calsc_utils::hash::HashedString;
 
-use crate::{FieldHavingType, base::instance::BaseTypeInstance};
+use crate::{
+    FieldHavingType,
+    base::instance::BaseTypeInstance,
+    func::{DeclBlockAffectedType, TypeSignature, TypedFunction},
+};
 
 /// The actual type used for typing in Calscin. Allows for nested references and arrays with base types
 #[derive(PartialEq, Clone, Debug)]
@@ -40,5 +45,36 @@ impl FieldHavingType for Type {
             Self::Reference { mutable: _, inner } => inner.has_field(name),
             Self::Base(instance) => instance.has_field(name),
         }
+    }
+}
+
+impl DeclBlockAffectedType for Type {
+    fn has_function(&self, name: HashedString) -> bool {
+        match self {
+            Self::Array { .. } => false,
+            Self::TypeParameter { .. } => false,
+            Self::Reference { mutable: _, inner } => inner.has_function(name),
+            Self::Base(instance) => instance.has_field(name),
+        }
+    }
+
+    fn get_func_signature(&self, name: HashedString) -> TypeSignature {
+        match self {
+            Self::Array { .. } => panic!("Cannot find function!"),
+            Self::TypeParameter { .. } => panic!("Cannot find function!"),
+            Self::Reference { mutable: _, inner } => inner.get_func_signature(name),
+            Self::Base(instance) => instance.get_func_signature(name),
+        }
+    }
+
+    fn add_function<K: DiagnosticSource>(
+        &mut self,
+        _name: HashedString,
+        _func: TypedFunction,
+        _source: &K,
+    ) -> DiagPossible {
+        panic!(
+            "Cannot add function directly from a Type tree instance. Use a BaseTypeInstance to add functions onto a type"
+        )
     }
 }
