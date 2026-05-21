7cd5b8f19b9e33f1b84a783d5a6549cd38ed9ff1
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-21 17:39:43 +0200

    feat: added the base of the local context

--- compiler/calsc_hir/src/lib.rs
+++ compiler/calsc_hir/src/lib.rs
@@ -1,5 +1,6 @@
 //! The main HIR declarations of Calscin. HIR is used to lower the AST into a guaranteed working form.
 
 pub mod globalctx;
+pub mod localctx;
 pub mod nodes;
 pub mod refs;
---
+++ compiler/calsc_hir/src/localctx/mod.rs
@@ -0,0 +1,39 @@
+//! Definitions for the HIR local contexts
+//!
+//! Local contexts are used in functions to store the following:
+//! - Variables (start, end of eras)
+//! - Eras / branches
+//! - Finshing / ending points
+//!
+//! Eras are also named branch indexes
+
+use std::collections::{HashMap, HashSet};
+
+use calsc_typing::tree::Type;
+use calsc_utils::hash::HashedString;
+
+pub struct LocalContext {
+    pub name: HashedString,
+
+    /// Main hashmap to convert a string into an actual variable vector index
+    pub hash_to_ind: HashMap<HashedString, usize>,
+
+    /// Stores whenever each branch ends. If a branch is contained here, it ended.
+    pub branch_ends: HashSet<usize>,
+
+    pub return_type: Option<Type>,
+
+    pub current_branch: usize,
+}
+
+impl LocalContext {
+    pub fn new(name: HashedString, return_type: Option<Type>) -> Self {
+        Self {
+            name,
+            hash_to_ind: HashMap::new(),
+            branch_ends: HashSet::new(),
+            return_type,
+            current_branch: 0,
+        }
+    }
+}
