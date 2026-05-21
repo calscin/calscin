9b080bb0c62ab166e3ec89bbc636b590d121ac45
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-21 13:32:59 +0200

    feat: added global scope storage

--- compiler/calsc_hir/src/globalctx/key.rs
+++ compiler/calsc_hir/src/globalctx/key.rs
@@ -1,10 +1,14 @@
 //! Definitions for the Global context keys.
 
-use std::hash::{Hash, Hasher};
+use std::{
+    fmt::Display,
+    hash::{Hash, Hasher},
+};
 
 use calsc_utils::hash::HashedString;
 
 /// The key to an entry in the global ctx
+#[derive(PartialEq, Eq)]
 pub struct GlobalContextKey {
     name: HashedString,
 }
@@ -20,5 +24,11 @@ impl Hash for GlobalContextKey {
     fn hash<H: Hasher>(&self, hasher: &mut H) {
         hasher.write_usize(1); // Marker for HIR type values to avoid collisions with hashes from HashedString
         hasher.write_u64(self.name.hash());
+    }
+}
+
+impl Display for GlobalContextKey {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", *self.name)
     }
 }
--- compiler/calsc_hir/src/globalctx/mod.rs
+++ compiler/calsc_hir/src/globalctx/mod.rs
@@ -3,5 +3,43 @@
 //! - Types ([`BaseTypeInstance`][`calsc_typing::base::instance::BaseTypeInstance`])
 //! - Functions (signatures, local contexts, declaration references)
 
+use std::collections::HashMap;
+
+use calsc_diagnostics::{DiagResult, DiagnosticSource, diags::errors::build_already_in_scope};
+
+use crate::globalctx::{key::GlobalContextKey, vals::GlobalContextValue};
+
 pub mod key;
 pub mod vals;
+
+pub struct GlobalContext {
+    key_to_ind: HashMap<GlobalContextKey, usize>,
+    pub values: Vec<GlobalContextValue>,
+}
+
+impl GlobalContext {
+    pub fn new() -> Self {
+        Self {
+            key_to_ind: HashMap::new(),
+            values: vec![],
+        }
+    }
+
+    pub fn append<K: DiagnosticSource>(
+        &mut self,
+        key: GlobalContextKey,
+        value: GlobalContextValue,
+        origin: &K,
+    ) -> DiagResult<usize> {
+        if self.key_to_ind.contains_key(&key) {
+            return Err(build_already_in_scope(&key, origin).into());
+        }
+
+        let ind = self.values.len();
+
+        self.key_to_ind.insert(key, ind);
+        self.values.push(value);
+
+        Ok(ind)
+    }
+}
