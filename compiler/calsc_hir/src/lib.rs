e0449426f05ff40df33b1be7fe3aec7e6321ca04
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-23 11:29:43 +0200

    feat: added deref to HIR node refs

--- compiler/calsc_hir/src/lib.rs
+++ compiler/calsc_hir/src/lib.rs
@@ -1,7 +1,31 @@
 //! The main HIR declarations of Calscin. HIR is used to lower the AST into a guaranteed working form.
 
+use std::cell::RefCell;
+
+use calsc_utils::alloc::arena::ArenaAllocator;
+
+use crate::{globalctx::GlobalContext, nodes::HIRNode, refs::HIRArenaReference};
+
 pub mod funcs;
 pub mod globalctx;
 pub mod localctx;
 pub mod nodes;
 pub mod refs;
+
+thread_local! {
+    static HIR_CONTEXT: RefCell<HIRContext> = RefCell::new(HIRContext::new());
+}
+
+pub struct HIRContext {
+    pub nodes: ArenaAllocator<HIRNode, HIRArenaReference>,
+    pub scope: GlobalContext,
+}
+
+impl HIRContext {
+    pub fn new() -> Self {
+        Self {
+            nodes: ArenaAllocator::new(),
+            scope: GlobalContext::new(),
+        }
+    }
+}
--- compiler/calsc_hir/src/refs.rs
+++ compiler/calsc_hir/src/refs.rs
@@ -1,4 +1,8 @@
+use std::ops::Deref;
+
 use calsc_utils::alloc::arena::ArenaAllocatorReference;
+
+use crate::{HIR_CONTEXT, nodes::HIRNode};
 
 #[must_use]
 #[derive(PartialEq, Clone)]
@@ -15,5 +19,13 @@ impl From<ArenaAllocatorReference> for HIRArenaReference {
 impl From<HIRArenaReference> for ArenaAllocatorReference {
     fn from(value: HIRArenaReference) -> Self {
         value.refer
+    }
+}
+
+impl Deref for HIRArenaReference {
+    type Target = HIRNode;
+
+    fn deref(&self) -> &Self::Target {
+        HIR_CONTEXT.with_borrow(|f| f.nodes.get_static(self.clone()))
     }
 }
