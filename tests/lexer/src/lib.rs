812b1b3ed2198062cad52d1e90ea9f8534af1c6e
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-24 16:16:15 +0200

    chore: moved lexer tests

---
+++ tests/lexer/src/lib.rs
@@ -0,0 +1,50 @@
+#[cfg(test)]
+use calsc_lexer::{lexer_tokenize, toks::TokenKind};
+
+#[test]
+fn parse_float_token() {
+    let tokens = lexer_tokenize("2.5844744", "test".to_string()).unwrap();
+
+    assert!(tokens[0].is_float_lit());
+    assert_eq!(tokens[0].kind, TokenKind::FloatLiteral(2.5844744));
+}
+
+#[test]
+fn parse_int_token() {
+    let tokens = lexer_tokenize("123456", "test".to_string()).unwrap();
+
+    assert!(tokens[0].is_int_lit());
+    assert_eq!(tokens[0].kind, TokenKind::IntLiteral(123456));
+}
+
+#[test]
+fn parse_int_float_token() {
+    let tokens = lexer_tokenize("1.123 456", "test".to_string()).unwrap();
+
+    assert!(tokens[0].is_float_lit());
+    assert!(tokens[1].is_int_lit());
+    assert_eq!(tokens[0].kind, TokenKind::FloatLiteral(1.123));
+    assert_eq!(tokens[1].kind, TokenKind::IntLiteral(456));
+}
+
+#[test]
+fn parse_keyword_token() {
+    let tokens = lexer_tokenize("abcdef", "test".to_string()).unwrap();
+
+    assert!(tokens[0].is_keyword());
+    assert_eq!(tokens[0].kind, TokenKind::Keyword("abcdef".to_string()));
+}
+
+#[test]
+fn parse_hashed_keyword_token() {
+    let tokens = lexer_tokenize("func", "test".to_string()).unwrap();
+
+    assert!(tokens[0].kind == TokenKind::Function);
+}
+
+#[test]
+fn parse_broken_float_literal_token() {
+    let tokens = lexer_tokenize("1.2.3.54", "test".to_string());
+
+    assert!(tokens.is_err());
+}
