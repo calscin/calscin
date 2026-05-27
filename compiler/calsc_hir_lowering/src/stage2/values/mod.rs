2f1817cbac51400d1db70d0bce7dae7afd2e2741
Zffu <103074097+Zffu@users.noreply.github.com>
2026-05-27 19:21:41 +0200

    feat: added stage 2 range lowering

--- compiler/calsc_hir_lowering/src/stage2/values/mod.rs
+++ compiler/calsc_hir_lowering/src/stage2/values/mod.rs
@@ -3,8 +3,12 @@
 use std::hint::unreachable_unchecked;
 
 use calsc_ast::nodes::{ASTNode, ASTNodeKind};
-use calsc_diagnostics::DiagResult;
-use calsc_hir::{localctx::LocalContext, refs::HIRArenaReference};
+use calsc_diagnostics::{DiagResult, diags::errors::build_expected_error};
+use calsc_hir::{
+    localctx::LocalContext,
+    nodes::{HIRNode, HIRNodeKind},
+    refs::HIRArenaReference,
+};
 
 use crate::stage2::values::{
     booleans::lower_hir_inverse_condition,
@@ -33,6 +37,61 @@ pub fn lower_ast_value(
         ASTNodeKind::PointerReference(_) => lower_ast_pointer_reference(node, local_ctx),
         ASTNodeKind::PointerDereference(_) => lower_ast_pointer_dereference(node, local_ctx),
 
+        ASTNodeKind::Range { .. } => lower_ast_range(node, local_ctx),
+
         _ => unsafe { unreachable_unchecked() },
+    }
+}
+
+pub fn lower_ast_range(
+    node: ASTNode,
+    local_ctx: Option<&LocalContext>,
+) -> DiagResult<HIRArenaReference> {
+    if let ASTNodeKind::Range {
+        start,
+        end,
+        increment,
+    } = &node.kind
+    {
+        let start = lower_ast_value(ASTNode::clone(start), local_ctx)?;
+        let start_type = start.get_type(local_ctx)?;
+
+        if start_type.is_none() {
+            return Err(build_expected_error(
+                &"non void element".to_string(),
+                &"void element".to_string(),
+                &*start,
+            )
+            .into());
+        }
+
+        let start_type = start_type.unwrap();
+
+        let end = lower_ast_value(ASTNode::clone(end), local_ctx)?
+            .use_as(start_type.clone(), local_ctx)?
+            .push();
+
+        let mut incr = None;
+
+        if increment.is_some() {
+            incr = Some(lower_ast_value(
+                ASTNode::clone(&increment.clone().unwrap()),
+                local_ctx,
+            )?);
+        }
+
+        let node = HIRNode::new(
+            HIRNodeKind::Range {
+                start,
+                end,
+                increment: incr,
+            },
+            node.start.clone(),
+            node.end.clone(),
+        );
+
+        Ok(node.push())
+    } else {
+        unsafe { unreachable_unchecked() }
     }
 }
