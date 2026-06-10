116c2a3eb76640f8afe84a36fa87d3c94ff68970
Zffu <103074097+Zffu@users.noreply.github.com>
2026-06-10 13:20:14 +0200

    feat: finished build command

--- compiler/calsc_builder/src/lib.rs
+++ compiler/calsc_builder/src/lib.rs
@@ -1,13 +1,17 @@
-use std::{fs, path::PathBuf, process};
+use std::{
+    fs,
+    path::PathBuf,
+    process::{self, Command},
+};
 
 use calsc_ast::{AST_CONTEXT, parser::ctx::parse_ast_whole};
-use calsc_diagnostics::{DiagPossible, container::dump_and_stop_if_errors};
+use calsc_diagnostics::container::dump_and_stop_if_errors;
 use calsc_hir::HIR_CONTEXT;
 use calsc_hir_lowering::{stage1::lower_hir_stage_1, stage2::lower_hir_stage_2};
 use calsc_lexer::lexer_tokenize;
 use calsc_remir_lowering::build_context_to_object_file;
 
-pub fn build_file(path: PathBuf, out: PathBuf) -> DiagPossible {
+pub fn build_file(path: PathBuf, out: PathBuf) {
     let module_name = path.file_name().unwrap().to_str().unwrap().to_string();
 
     let contents = match fs::read_to_string(path.clone()) {
@@ -46,6 +50,14 @@ pub fn build_file(path: PathBuf, out: PathBuf) -> DiagPossible {
 
     let _ = build_context_to_object_file(hir, module_name, out);
     dump_and_stop_if_errors();
+}
 
-    Ok(())
+pub fn link_files(files: Vec<PathBuf>, out: PathBuf, linker: String) {
+    let mut command = Command::new(linker);
+
+    for file in files {
+        command.arg(file.to_str().unwrap());
+    }
+
+    command.arg(format!("-o {}", out.to_str().unwrap()));
 }
--- compiler/cli/src/commands/build.rs
+++ compiler/cli/src/commands/build.rs
@@ -1,9 +1,11 @@
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
 
+use calsc_builder::{build_file, link_files};
 use indicatif::{ProgressBar, ProgressStyle};
 
 pub fn build_command(input: Vec<PathBuf>, out: PathBuf, linker: String) {
     let progress_bar = ProgressBar::new(input.len() as u64);
+    let mut object_files = vec![];
 
     progress_bar.set_style(
         ProgressStyle::with_template(
@@ -13,4 +15,23 @@ pub fn build_command(input: Vec<PathBuf>, out: PathBuf, linker: String) {
         .unwrap()
         .progress_chars("=>"),
     );
+
+    let mut ind = 0;
+    for path in input {
+        let out_object_file = path.with_extension("o");
+
+        build_file(path, out_object_file.clone());
+
+        object_files.push(out_object_file);
+    }
+
+    progress_bar.finish_with_message("Linking");
+
+    link_files(object_files, out, linker);
+
+    progress_bar.finish_with_message("Cleaning up");
+
+    for object in object_files {
+        fs::read(object);
+    }
 }
