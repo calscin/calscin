4b6753f883ac16f190198a4cac3db88ac625cedc
Zffu <103074097+Zffu@users.noreply.github.com>
2026-06-16 20:12:22 +0200

    feat: added visibility

---
+++ compiler/calsc_modules/src/visibility.rs
@@ -0,0 +1,33 @@
+//! The visibility in Calscin. Represents the visibility of an element inside of the HIR tree for example.
+//! Visibility in Calscin has three levels:
+//!
+//! - **Public**: The element is accessible by anyone
+//! - **Protected**: The element is only accessible inside of the package
+//! - **Private**: The element is only accessible inside of the module
+
+use crate::path::ModulePath;
+
+/// The visibility enum
+pub enum Visibility {
+    /// The element is accessible by anyone
+    Public,
+
+    /// The element is only accessible inside of the package
+    Protected(ModulePath),
+
+    /// The element is only accessible inside of the module
+    Private(ModulePath),
+}
+
+impl Visibility {
+    /// Can the element be viewed / accessed by the given module path.
+    pub fn can_view(&self, path: &ModulePath) -> bool {
+        match self {
+            Self::Public => true,
+            Self::Protected(inner_path) => path.package == inner_path.package,
+            Self::Private(inner_path) => {
+                path.package == inner_path.package && path.path == inner_path.path
+            }
+        }
+    }
+}
