9db1f44cfc3ba5314953275792fed684d3be830c
Zffu <103074097+Zffu@users.noreply.github.com>
2026-06-22 15:52:29 +0200

    feat: added more intrinsics

--- compiler/calsc_hir/src/intrinsics.rs
+++ compiler/calsc_hir/src/intrinsics.rs
@@ -6,8 +6,30 @@ pub enum Intrinsics {
     Alloca,
 
     /// Allocates a specific amount of bytes in the heap
+    /// `std::intrinsics::heap_alloca`
     HeapAlloca,
 
     /// Frees a heap allocated pointer
+    /// `std::intrinsics::free`
     Free,
+
+    /// Loads a pointer atomically
+    /// `std::intrinsics::load_atomic`
+    LoadAtomic,
+
+    /// Stores into a pointer atomically
+    /// `std::intrinsics::store_atomic`
+    StoreAtomic,
+
+    /// Selects a value based on the output of the condition
+    /// `std::intrinsics::select`
+    Select,
+
+    /// Assumes a boolean value is true
+    /// `std::intrinsics::assume`
+    Assume,
+
+    /// Assumes a place is unreachable
+    /// `std::intrinsics::unreachable`
+    Unreachable,
 }
