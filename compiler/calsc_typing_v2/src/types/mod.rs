d3cd0db7452a4fdf90b25018114b0f9a305cdf28
Zffu <103074097+Zffu@users.noreply.github.com>
2026-06-22 18:25:34 +0200

    feat: finished TypeKind enum for now

--- compiler/calsc_typing_v2/src/types/mod.rs
+++ compiler/calsc_typing_v2/src/types/mod.rs
@@ -5,6 +5,7 @@ use calsc_utils::alloc::arena::ArenaHandle;
 /// The state of mutation of a type.
 /// A false value represents that the type is immutable.
 /// A true value represents that the type is mutable.
+#[derive(PartialEq, Eq, Hash)]
 pub struct MutationState(pub bool);
 
 /// The kind of type. Represents types. Uses the arena allocator to contain inner types
@@ -26,4 +27,18 @@ pub enum TypeKind {
     /// The handle represents a [`TypeKind`]
     ///
     Pointer(MutationState, ArenaHandle),
+
+    /// Represents a compile-time sized array.
+    ///
+    /// # Example
+    /// `s.32[32]` is a 32-sized array of type `s.32`
+    ///
+    /// The handle represents a [`TypeKind`]
+    ///
+    Array(usize, ArenaHandle),
+
+    /// A segment represents a continuous segment of memory that has an array-like representation.
+    ///
+    /// The handle represents a [`TypeKind`]
+    Segment(ArenaHandle),
 }
