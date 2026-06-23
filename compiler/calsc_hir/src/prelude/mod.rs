e12ad8764a71a5c8a7c885b94db1be0e434d7bb6
Zffu <103074097+Zffu@users.noreply.github.com>
2026-06-23 18:01:20 +0200

    feat: added PreludeApplier onto GlobalContext

--- compiler/calsc_hir/src/prelude/mod.rs
+++ compiler/calsc_hir/src/prelude/mod.rs
@@ -2,77 +2,27 @@
 
 use calsc_diagnostics::{DiagPossible, DiagnosticSource};
 use calsc_modules::{path::ModulePath, visibility::Visibility};
-use calsc_typing::base::{BaseType, kind::BaseTypeKind};
+use calsc_typing_v2::{traits::PreludeApplier, types::primitive::PrimitiveType};
+use calsc_utils::hash::HashedString;
 
 use crate::globalctx::{GlobalContext, key::GlobalContextKey, vals::GlobalContextValue};
 
-/// Applies the HIR prelude to the given scope
-pub fn apply_prelude<K: DiagnosticSource>(scope: &mut GlobalContext, origin: &K) -> DiagPossible {
-    let module_path = ModulePath::new_prelude_path(vec!["types".into()]);
+impl PreludeApplier for GlobalContext {
+    fn register_type<K: DiagnosticSource>(
+        &mut self,
+        name: HashedString,
+        ty: PrimitiveType,
+        source: &K,
+    ) -> DiagPossible {
+        let module_path = ModulePath::new_prelude_path(vec!["types".into()]);
 
-    let bool_type = GlobalContextValue::new_type(BaseType::new(BaseTypeKind::Boolean));
+        self.append(
+            GlobalContextKey::new(name).module_path(module_path),
+            GlobalContextValue::Type(ty),
+            Visibility::Uncopiable,
+            source,
+        )?;
 
-    let signed_integer_type =
-        GlobalContextValue::new_type(BaseType::new(BaseTypeKind::Integer { signed: true }));
-    let unsigned_integer_type =
-        GlobalContextValue::new_type(BaseType::new(BaseTypeKind::Integer { signed: true }));
-
-    let signed_float_type =
-        GlobalContextValue::new_type(BaseType::new(BaseTypeKind::Floating { signed: true }));
-
-    let unsigned_float_type =
-        GlobalContextValue::new_type(BaseType::new(BaseTypeKind::Floating { signed: false }));
-
-    let string_type = GlobalContextValue::new_type(BaseType::new(BaseTypeKind::String));
-    let char_type = GlobalContextValue::new_type(BaseType::new(BaseTypeKind::Char));
-
-    scope.append(
-        GlobalContextKey::new("bool".into()),
-        bool_type,
-        Visibility::Uncopiable,
-        origin,
-    )?;
-
-    scope.append(
-        GlobalContextKey::new("s".into()).module_path(module_path.clone()),
-        signed_integer_type,
-        Visibility::Uncopiable,
-        origin,
-    )?;
-
-    scope.append(
-        GlobalContextKey::new("u".into()).module_path(module_path.clone()),
-        unsigned_integer_type,
-        Visibility::Uncopiable,
-        origin,
-    )?;
-
-    scope.append(
-        GlobalContextKey::new("f".into()).module_path(module_path.clone()),
-        signed_float_type,
-        Visibility::Uncopiable,
-        origin,
-    )?;
-
-    scope.append(
-        GlobalContextKey::new("uf".into()).module_path(module_path.clone()),
-        unsigned_float_type,
-        Visibility::Uncopiable,
-        origin,
-    )?;
-
-    scope.append(
-        GlobalContextKey::new("str".into()).module_path(module_path.clone()),
-        string_type,
-        Visibility::Uncopiable,
-        origin,
-    )?;
-    scope.append(
-        GlobalContextKey::new("char".into()).module_path(module_path.clone()),
-        char_type,
-        Visibility::Uncopiable,
-        origin,
-    )?;
-
-    Ok(())
+        Ok(())
+    }
 }
