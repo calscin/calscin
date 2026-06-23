d678637ab969cddd4547a68b2f4710df8c92fd6d
Zffu <103074097+Zffu@users.noreply.github.com>
2026-06-23 16:48:56 +0200

    feat: added type transmutation and casting for type kinds

--- compiler/calsc_typing_v2/src/into/primitives.rs
+++ compiler/calsc_typing_v2/src/into/primitives.rs
@@ -1,12 +1,13 @@
 //! Type convertions for primitives
 
 use crate::{
+    ctx::TypeCtx,
     into::{TypeCasting, TypeTransmutation},
     types::primitive::PrimitiveType,
 };
 
 impl TypeTransmutation for PrimitiveType {
-    fn can_transmute(&self, into: &Self) -> bool {
+    fn can_transmute(&self, into: &Self, _ctx: &TypeCtx) -> bool {
         if self == into {
             return true;
         }
@@ -18,8 +19,8 @@ impl TypeTransmutation for PrimitiveType {
         }
     }
 
-    fn can_transmute_weakly(&self, into: &Self) -> bool {
-        if self.can_transmute(into) {
+    fn can_transmute_weakly(&self, into: &Self, ctx: &TypeCtx) -> bool {
+        if self.can_transmute(into, ctx) {
             return true;
         }
 
@@ -32,7 +33,7 @@ impl TypeTransmutation for PrimitiveType {
 }
 
 impl TypeCasting for PrimitiveType {
-    fn can_cast(&self, into: &Self) -> bool {
+    fn can_cast(&self, into: &Self, _ctx: &TypeCtx) -> bool {
         if self == into {
             return true;
         }
