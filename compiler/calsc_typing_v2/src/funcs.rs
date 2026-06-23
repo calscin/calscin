80f9fad0b0bf6726c702359440aa5aaa97a64d0f
Zffu <103074097+Zffu@users.noreply.github.com>
2026-06-23 16:00:00 +0200

    feat: removed NamedFunction as the typing system should not have any name information

--- compiler/calsc_typing_v2/src/funcs.rs
+++ compiler/calsc_typing_v2/src/funcs.rs
@@ -1,25 +1,9 @@
-use calsc_utils::hash::HashedString;
-
 use crate::types::TypeKind;
 
-/// Represents a function with a name.
-pub struct NamedFunction {
-    pub name: HashedString,
-    pub return_type: TypeKind,
-    pub arguments: Vec<TypeKind>,
-}
-
-/// Represents a function without a name
-pub struct UnNamedFunction {
+/// Represents a function inside of the type system.
+/// Functions within the type system should not hold any name / module path since it is the HIR job to do that.
+#[derive(PartialEq, Clone)]
+pub struct TypedFunction {
     pub return_type: TypeKind,
     pub argument: Vec<TypeKind>,
-}
-
-impl Into<UnNamedFunction> for NamedFunction {
-    fn into(self) -> UnNamedFunction {
-        UnNamedFunction {
-            return_type: self.return_type,
-            argument: self.arguments,
-        }
-    }
 }
