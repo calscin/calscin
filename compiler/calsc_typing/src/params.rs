a9cbc6b027bbd6d761c05cebe82f333f03b97e9f
Zffu <103074097+Zffu@users.noreply.github.com>
2026-06-25 20:04:16 +0200

    feat: finished type param ctx

--- compiler/calsc_typing/src/params.rs
+++ compiler/calsc_typing/src/params.rs
@@ -2,6 +2,10 @@
 
 use std::collections::HashMap;
 
+use calsc_diagnostics::{
+    DiagResult, DiagnosticSource,
+    diags::errors::{build_already_in_scope, build_cannot_find_element_no_closest},
+};
 use calsc_utils::hash::HashedString;
 
 /// This is a safe handle from a type parameter stored inside of a [`TypeParamCtx`] this enforces that type parameters go trough the expected path.
@@ -21,5 +25,47 @@ impl TypeParamCtx {
         Self {
             params: HashMap::new(),
         }
+    }
+
+    /// Gets the type parameter from the name
+    ///
+    /// # Errors
+    /// This function will error if the type parameter doesn't exist, to avoid this, use [`TypeParamCtx::has_type_parameter`]
+    ///
+    pub fn get_type_param<S: DiagnosticSource>(
+        &self,
+        name: &HashedString,
+        source: &S,
+    ) -> DiagResult<TypeParameterId> {
+        if !self.params.contains_key(name) {
+            return Err(build_cannot_find_element_no_closest(&name, source).into());
+        }
+
+        Ok(TypeParameterId(self.params[name].id))
+    }
+
+    /// Appends a type parameter inside of the context
+    ///
+    /// # Errors
+    /// This function will error if a type parameter with the given name already exists.
+    ///
+    pub fn append_type_param<S: DiagnosticSource>(
+        &mut self,
+        name: HashedString,
+        source: &S,
+    ) -> DiagResult<TypeParameterId> {
+        if self.params.contains_key(&name) {
+            return Err(build_already_in_scope(&name, source).into());
+        }
+
+        let id = self.params.len();
+
+        self.params.insert(name.clone(), HeldTypeParam { name, id });
+
+        Ok(TypeParameterId(id))
+    }
+
+    pub fn has_type_parameter(&self, name: &HashedString) -> bool {
+        self.params.contains_key(name)
     }
 }
