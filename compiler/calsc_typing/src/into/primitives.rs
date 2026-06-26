606a0e120f0c4f24d1fd90ba84254f08069edb9f
Zffu <103074097+Zffu@users.noreply.github.com>
2026-06-26 09:05:31 +0200

    feat: removed type parameter transmutation for Any -> TypeParam

--- compiler/calsc_typing/src/into/primitives.rs
+++ compiler/calsc_typing/src/into/primitives.rs
@@ -14,7 +14,6 @@ impl TypeTransmutation for PrimitiveType {
 
         match (self, into) {
             (PrimitiveType::Int(signed), PrimitiveType::Int(_)) => !signed, // Allow unsigned -> signed convertion
-            (_, PrimitiveType::TypeParameter(_)) => true,
 
             _ => false,
         }
