77e7bf781ed198d39e82f4ce8fbd8531c76f150f
Zffu <103074097+Zffu@users.noreply.github.com>
2026-06-28 13:31:11 +0200

    feat: added typed funtions from lazy loaded types

--- compiler/calsc_modules/src/lazy/raw.rs
+++ compiler/calsc_modules/src/lazy/raw.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-use calsc_diagnostics::{DiagPossible, DiagnosticSource, diags::errors::build_already_in_scope};
+use calsc_diagnostics::{DiagPossible, DiagnosticSource};
 use calsc_utils::hash::{HashedCounter, HashedString};
 
 use crate::{
@@ -22,32 +22,11 @@ pub enum LazyLoadedRawTypeKind {
 #[derive(Debug, Clone)]
 pub struct LazyLoadedRawType {
     pub kind: LazyLoadedRawTypeKind,
-
-    pub functions: HashMap<HashedString, (Vec<(HashedString, LazyLoadedType)>, LazyLoadedType)>,
 }
 
 impl LazyLoadedRawType {
     pub fn new(kind: LazyLoadedRawTypeKind) -> Self {
-        Self {
-            kind,
-            functions: HashMap::new(),
-        }
-    }
-
-    pub fn append_function<S: DiagnosticSource>(
-        &mut self,
-        name: HashedString,
-        ret_type: LazyLoadedType,
-        arguments: Vec<(HashedString, LazyLoadedType)>,
-        source: &S,
-    ) -> DiagPossible {
-        if self.functions.contains_key(&name) {
-            return Err(build_already_in_scope(&name, source).into());
-        }
-
-        self.functions.insert(name, (arguments, ret_type));
-
-        Ok(())
+        Self { kind }
     }
 }
 
