63f808f47fbe1cd9d490305b5d2a011b04b316e1
Zffu <103074097+Zffu@users.noreply.github.com>
2026-07-01 01:04:42 +0200

    feat: added basic module tree

--- compiler/calsc_modules/src/treev2/mod.rs
+++ compiler/calsc_modules/src/treev2/mod.rs
@@ -1,4 +1,34 @@
 //! The second version of the Calscin module tree.
 
+use std::{
+    collections::{HashMap, HashSet},
+    path::PathBuf,
+};
+
+use calsc_utils::{alloc::arena::ArenaAllocator, hash::HashedString};
+
+use crate::{
+    path::ModulePath,
+    treev2::{entry::TreeEntry, module::TreeModule},
+};
+
 pub mod entry;
 pub mod module;
+
+pub struct ModuleTree {
+    pub children: HashMap<HashedString, TreeEntry>,
+    pub resolved_cache: HashMap<ModulePath, TreeEntry>,
+    pub module_arena: ArenaAllocator<TreeModule>,
+    pub used_files: HashSet<PathBuf>,
+}
+
+impl ModuleTree {
+    pub fn new() -> Self {
+        Self {
+            children: HashMap::new(),
+            resolved_cache: HashMap::new(),
+            module_arena: ArenaAllocator::new(),
+            used_files: HashSet::new(),
+        }
+    }
+}
