f715aa29548bb240ae22f411ea19d639bc578bdd
Zffu <103074097+Zffu@users.noreply.github.com>
2026-07-01 01:08:09 +0200

    feat: added typing and semantic dependencies on Tree entries

--- compiler/calsc_modules/src/treev2/entry.rs
+++ compiler/calsc_modules/src/treev2/entry.rs
@@ -1,7 +1,31 @@
+use std::collections::HashSet;
+
 use calsc_utils::alloc::arena::ArenaHandle;
 
-pub enum TreeEntry {
+use crate::path::ModulePath;
+
+pub enum TreeEntryKind {
     Type,
     Function,
     Module(ArenaHandle),
+}
+
+pub struct TreeEntry {
+    pub self_path: ModulePath,
+
+    pub kind: TreeEntryKind,
+
+    pub typing_dependencies: HashSet<ModulePath>,
+    pub semantic_dependencies: HashSet<ModulePath>,
+}
+
+impl TreeEntry {
+    pub fn new(kind: TreeEntryKind, self_path: ModulePath) -> Self {
+        Self {
+            kind,
+            self_path,
+            typing_dependencies: HashSet::new(),
+            semantic_dependencies: HashSet::new(),
+        }
+    }
 }
