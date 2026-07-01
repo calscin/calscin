e6e9c081103911ae63ed1c5945b8df713cfdb931
Zffu <103074097+Zffu@users.noreply.github.com>
2026-07-01 22:02:23 +0200

    feat: added layer description

--- compiler/calsc_tree_build/src/lib.rs
+++ compiler/calsc_tree_build/src/lib.rs
@@ -1,1 +1,13 @@
-
+//! This layer runs right after the *AST* and is mostly made to generate a module tree of the package, containing every used module in a tree structure.
+//! Each module containing the following information:
+//! - *Elements*: the children / symbols of the module. Can also contain child modules
+//! - *Imports*: the imports used inside of the module is a table of `imported element name / path -> true full path inside of the package`
+//! - *File*: the file that made this module / the file origin of the module.
+//!
+//! Furthermore the root of the module tree itself will store the following information:
+//! - Module tree
+//! - `Path -> Module Entry` resolved cache
+//! - Set of files that are used for every discovered modules (`HashSet<PathBuf>`)
+//!
+//!
+//! We also associate every entry with the related nodes, for example every type tree entry should be related to the AST node that caused it to be added / to exist.
