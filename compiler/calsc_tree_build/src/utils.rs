fbb901ba1ee1e08147971bb9add7dd14b6207a3f
Zffu <103074097+Zffu@users.noreply.github.com>
2026-07-03 14:06:07 +0200

    feat: made the util function better

--- compiler/calsc_tree_build/src/utils.rs
+++ compiler/calsc_tree_build/src/utils.rs
@@ -4,16 +4,16 @@ use calsc_utils::hash::HashedString;
 
 pub(crate) fn get_module_name_from_file(file: &PathBuf) -> HashedString {
     let file = if file.file_name() == Some(OsStr::new("module.cal")) {
-        &file.parent().unwrap().to_path_buf()
+        &file.parent().expect("not relative path").to_path_buf()
     } else {
         file
     };
 
     file.with_extension("")
         .file_name()
-        .unwrap()
+        .expect("A path not ending by ..")
         .to_str()
-        .unwrap()
+        .expect("The file name is not valid unicode")
         .to_string()
         .into()
 }
