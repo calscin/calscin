87f99f7fb6e0de7e90fdf9ce04dfb47c1e92b26a
Zffu <103074097+Zffu@users.noreply.github.com>
2026-07-04 13:46:35 +0200

    feat: added as_module and as_module_mut

--- compiler/calsc_modules/src/treev2/entry.rs
+++ compiler/calsc_modules/src/treev2/entry.rs
@@ -1,4 +1,6 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, fmt::Display};
+
+use calsc_diagnostics::{DiagResult, DiagnosticSource, diags::errors::build_expected_entry_type};
 
 use crate::{path::ModulePath, treev2::module::TreeModule};
 
@@ -44,5 +46,54 @@ impl TreeEntry {
 
     pub fn has_dependency(&self, dep: ModulePath) -> bool {
         self.typing_dependencies.contains(&dep) || self.semantic_dependencies.contains(&dep)
+    }
+
+    pub fn has_related_nodes(&self) -> bool {
+        matches!(self.kind, TreeEntryKind::Function | TreeEntryKind::Type)
+    }
+}
+
+impl TreeEntryKind {
+    #[inline]
+    pub fn is_module(&self) -> bool {
+        matches!(self, Self::Module(_))
+    }
+
+    pub fn as_module<'a, S: DiagnosticSource>(&'a self, source: &S) -> DiagResult<&'a TreeModule> {
+        if !self.is_module() {
+            return Err(build_expected_entry_type(&"module".to_string(), self, source).into());
+        }
+
+        if let Self::Module(module) = self {
+            return Ok(module);
+        } else {
+            unreachable!()
+        }
+    }
+
+    pub fn as_module_mut<'a, S: DiagnosticSource>(
+        &'a mut self,
+        source: &S,
+    ) -> DiagResult<&'a mut TreeModule> {
+        if !self.is_module() {
+            return Err(build_expected_entry_type(&"module".to_string(), self, source).into());
+        }
+
+        if let Self::Module(module) = self {
+            return Ok(module);
+        } else {
+            unreachable!()
+        }
+    }
+}
+
+impl Display for TreeEntryKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Module(module) => write!(f, "module ({})", module.name),
+            Self::Function => write!(f, "function"),
+            Self::Type => write!(f, "type"),
+            Self::PrimitiveType => write!(f, "primitive type"),
+        }
     }
 }
