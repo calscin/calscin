b586a0782b8a7eadecbadc2b1311295f3c16f879
Zffu <103074097+Zffu@users.noreply.github.com>
2026-07-09 20:09:46 +0200

    feat: added dependency getter and infinite size detection

--- compiler/calsc_tree_low/src/lib.rs
+++ compiler/calsc_tree_low/src/lib.rs
@@ -1,4 +1,11 @@
+use std::collections::HashSet;
+
+use calsc_diagnostics::{
+    DiagPossible, DiagResult, DiagnosticSource, diags::errors::build_type_infinite_size,
+};
 use calsc_modules::{path::ModulePath, visibility::Visibility};
+
+use crate::ctx::TreeLowCtx;
 
 pub mod ctx;
 pub mod lower;
@@ -16,4 +23,43 @@ pub fn convert_visibility(
         calsc_ast::visibility::Visibility::Protected => Visibility::Protected(parent),
         calsc_ast::visibility::Visibility::Private => Visibility::Private(parent),
     }
+}
+
+pub fn get_dependencies_of_entry<S: DiagnosticSource>(
+    ctx: &TreeLowCtx,
+    path: &ModulePath,
+    source: &S,
+) -> DiagResult<HashSet<ModulePath>> {
+    let mut set = HashSet::new();
+
+    get_dependencies_inner(ctx, &path, path, &mut set, source)?;
+
+    Ok(set)
+}
+
+fn get_dependencies_inner<S: DiagnosticSource>(
+    ctx: &TreeLowCtx,
+    master: &ModulePath,
+    path: &ModulePath,
+    set: &mut HashSet<ModulePath>,
+    source: &S,
+) -> DiagPossible {
+    let deps = ctx
+        .build_ctx
+        .tree
+        .get_entry(path, &ctx.build_ctx.arena, source)?
+        .get_dependencies()
+        .clone();
+
+    for dep in deps {
+        if &dep == master {
+            return Err(build_type_infinite_size(&dep, source).into());
+        }
+
+        get_dependencies_inner(ctx, master, &dep, set, source)?;
+
+        set.insert(dep);
+    }
+
+    Ok(())
 }
